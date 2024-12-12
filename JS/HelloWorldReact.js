161c827f73ba68dd6c317a8233ca46d621f2bd3d
sumasubbu <75.suma@gmail.com>
2024-12-12 23:41:57 +0530

    added code for chapters 1 and 2

--- JS/HelloWorldReact.js
+++ JS/HelloWorldReact.js
@@ -1,12 +1,14 @@
-const content = React.createElement('div',{id:'parent',key:'parent'},[
-  React.createElement('div',{id:'child1',key:'child1'},[
-    React.createElement('h1',{},'heading1'),
-    React.createElement('h1',{},'heading2')
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const content = React.createElement('div', { id: 'parent', key: 'parent' }, [
+  React.createElement('div', { id: 'child1', key: 'child1' }, [
+    React.createElement('h1', { key: 'child11' }, 'heading1'),
+    React.createElement('h1', { key: 'child12' }, 'heading2'),
   ]),
-  React.createElement('div',{id:'child2',key:'child2'},[
-    React.createElement('h1',{},'heading1'),
-    React.createElement('h1',{},'heading2')
-  ])
-])
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(content)
+  React.createElement('div', { id: 'child2', key: 'child2' }, [
+    React.createElement('h1', { key: 'child21' }, 'heading1'),
+    React.createElement('h1', { key: 'child22' }, 'heading2'),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(content);
